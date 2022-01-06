0502c27c9c71a9d4d86020a57b53cf4aef31967e
danghungtb26 <danghungtb26@gmail.com>
2022-01-06 10:13:46 +0700

    feat: add demo android

---
+++ SafemobileRNExambleAndroid/app/src/main/java/com/cyradar/safemobile_rn_examble_android/Main2Application.java
@@ -0,0 +1,8 @@
+package com.cyradar.safemobile_rn_examble_android;
+
+import android.app.Application;
+
+import com.rnproject.MainApplication;
+
+public class Main2Application extends MainApplication {
+}
